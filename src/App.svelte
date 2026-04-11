71bb8920bdf3b06602870d9ad6bacbbf72c17dd6
P-B <pranjalbaranwal304@gmail.com>
2026-04-11 23:28:23 +0530

    initialised npm and app.svelte

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount } from 'svelte'
+  import { storage } from '$lib/storage.js'
+
+  onMount(async () => {
+    await storage.init()
+    console.log('storage initialized')
+  })
+</script>
+
+<h1>Hello from Svelte</h1>
